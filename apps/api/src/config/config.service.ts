bc84bc6f77a412825633e34f8b23d6100f2f3070
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-11-05 20:40:22 +0200

    Feature: add app config

---
+++ apps/api/src/config/config.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService as NestConfigService } from '@nestjs/config';
+import { z } from 'zod';
+
+const envs = ['development', 'production'] as const;
+
+const validationObject = z.object({
+  APP_ENV: z.enum(envs),
+  APP_PORT: z.coerce.number().int().min(0).max(65535),
+  CLIENT_URL: z.url(),
+});
+
+@Injectable()
+export class ConfigService {
+  static validate = (
+    config: Record<string, unknown>,
+  ): z.infer<typeof validationObject> => validationObject.parse(config);
+
+  constructor(
+    private readonly configService: NestConfigService<
+      z.infer<typeof validationObject>,
+      true
+    >,
+  ) {}
+
+  get env(): (typeof envs)[number] {
+    return this.configService.get('APP_ENV');
+  }
+
+  get port(): number {
+    return Number(this.configService.get('APP_PORT'));
+  }
+
+  get clientUrl(): string {
+    return this.configService.get<string>('CLIENT_URL');
+  }
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 4000);
+  const { clientUrl, port } = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    credentials: true,
+    origin: [clientUrl],
+  });
+
+  await app.listen(port);
 }
-bootstrap();
+bootstrap().catch((e: unknown) => console.error(e));
