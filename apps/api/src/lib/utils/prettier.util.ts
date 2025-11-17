5df63cbdea550c7f719c37b5697d36c56d337bed
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-11-17 11:01:48 +0200

    Chore: refactor dto's script

---
+++ apps/api/src/lib/utils/prettier.util.ts
@@ -0,0 +1,10 @@
+import prettier from 'prettier';
+
+export async function formatWithPrettier(input: string, filePath: string) {
+  const prettierConfig = (await prettier.resolveConfig(filePath)) || {};
+
+  return prettier.format(input, {
+    ...prettierConfig,
+    parser: 'typescript',
+  });
+}
