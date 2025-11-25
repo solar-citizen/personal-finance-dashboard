19b48aeb8e86e7740900ad6587f1ec3ea2b51ed2
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-11-25 17:38:25 +0200

    Add another type

--- apps/web/src/components/absolute.tsx
+++ apps/web/src/components/absolute.tsx
@@ -12,6 +12,12 @@ type UserDataTwo = {
   age: any;
 };
 
+type UserDataThree = {
+  name?: string;
+  password: string;
+  age: any;
+};
+
 const API_URL = 'https://api.example.com';
 
 const u = 'john';
