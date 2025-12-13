f483fbcfb1d90099d9028d2ae511ebc0da41b8c8
Vladyslav Tishchenko <fsknnn@gmail.com>
2025-12-13 13:19:37 +0200

    Feature: add theme (light/dark)

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,9 +4,14 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import type { PropsWithChildren } from 'react';
 
+import { cn } from '#src/lib/utils';
+
 import { Providers } from './providers';
 
-const inter = Inter({ subsets: ['latin', 'cyrillic'] });
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'],
+  variable: '--font-inter',
+});
 
 export const metadata: Metadata = {
   title: 'Personal Finance Dashboard - Your Finance Manager',
@@ -16,7 +21,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang={'en'} suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={cn(inter.variable, 'antialiased')}>
         <Providers>{children}</Providers>
       </body>
     </html>
